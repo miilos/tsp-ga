6b0d2582311f49803984f8eca687c0c079c53ed2
Milos Popovic <milos.pop.004@gmail.com>
2026-06-27 17:05:53 +0200

    implement selection, crossover & mutation

--- ga.py
+++ ga.py
@@ -5,6 +5,8 @@ class Ga:
     POPULATION_SIZE = 10
     MAX_ITERATIONS = 100
 
+    TOURNAMENT_SIZE = 3
+
     def __init__(self, cities_provider: CitiesProvider):
         self.cities_provider = cities_provider
 
@@ -36,3 +38,55 @@ class Ga:
 
     def fitness(self, individual: list[int]) -> float:
         return 1 / self.calculate_cost(individual)
+
+    # tournament selection - works better than roulette wheel selection because
+    # all the tour lengths are too similar, so the odds of selecting the best vs. the worst
+    # individual aren't big enough
+    def selection(self, population: list[list[int]]) -> list[list[int]]:
+        new_population = []
+
+        for _ in range(self.POPULATION_SIZE):
+            tournament = random.sample(population, self.TOURNAMENT_SIZE)
+            winner = max(tournament, key=lambda individual: self.fitness(individual))
+            new_population.append(winner)
+
+        return new_population
+
+    # edge recombination crossover - build an adjacency list and only construct a child path
+    # from neighbors of its parents - results in the shortest edges
+    def crossover(self, parent1: list[int], parent2: list[int]) -> list[int]:
+        adjacency_list = {
+            city_id: self._neighbors(city_id, parent1) | self._neighbors(city_id, parent2)
+            for city_id in parent1
+        }
+
+        child = [random.choice(parent1)]
+        while len(child) < len(parent1):
+            curr_city_id = child[-1]
+            neighbors = adjacency_list[curr_city_id]
+
+            adjacency_list.pop(curr_city_id)
+            for neighbors_set in adjacency_list.values():
+                neighbors_set.discard(curr_city_id)
+
+            if neighbors:
+                min_neighbors = min([len(adjacency_list[city_id]) for city_id in neighbors])
+                next_candidates = [city_id for city_id in neighbors if len(adjacency_list[city_id]) == min_neighbors]
+                next_city = random.choice(next_candidates)
+            else:
+                next_city = random.choice([city_id for city_id in adjacency_list])
+
+            child.append(next_city)
+
+        return child
+
+    def _neighbors(self, city_id: int, individual: list[int]) -> set[int]:
+        city_idx = individual.index(city_id)
+        return { individual[city_idx-1], individual[(city_idx+1) % len(individual)] }
+
+    # reverse mutation - pick two random points and reverse cities between them
+    def mutate(self, individual: list[int]) -> list[int]:
+        individual = individual.copy()
+        start, end = sorted(random.sample(range(len(individual)), 2))
+        individual[start:end+1] = list(reversed(individual[start:end+1]))
+        return individual
--- ga_test.py
+++ ga_test.py
@@ -6,7 +6,19 @@ ga = Ga(cities_provider)
 
 population = ga.create_initial_population()
 
-for individual in population:
-    print("individual: ", individual)
-    print("cost: ", ga.calculate_cost(individual))
-    print("fitness: ", ga.fitness(individual))
+print(population[0])
+print(population[1])
+ga.crossover(population[0], population[1])
+
+# for individual in population:
+#     print("individual: ", individual)
+#     print("cost: ", ga.calculate_cost(individual))
+#     print("fitness: ", ga.fitness(individual))
+#
+# print("------------ new population ---------------")
+#
+# new_population = ga.selection(population)
+# for individual in new_population:
+#     print("individual: ", individual)
+#     print("cost: ", ga.calculate_cost(individual))
+#     print("fitness: ", ga.fitness(individual))
